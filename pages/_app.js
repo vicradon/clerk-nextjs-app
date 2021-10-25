58495d74f4651eef004daa33f44d30d9af68ccbb
Osinachi Chukwujama <osinachi.chukwujama@gmail.com>
2021-10-25 13:30:25 +0100

    chore: add package-lock.json

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
-import '../styles/globals.css'
-import { ClerkProvider, SignedIn, SignedOut } from '@clerk/nextjs'
-import { useRouter } from 'next/router'
-import Layout from '../components/Layout'
-import Head from 'next/head'
-import Script from 'next/script'
-import Link from 'next/link'
+import "../styles/globals.css";
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/nextjs";
+import { useRouter } from "next/router";
+import Layout from "../components/Layout";
+import Head from "next/head";
+import Script from "next/script";
+import Link from "next/link";
 
 /**
  * List pages you want to be publicly accessible, or leave empty if
@@ -14,10 +14,10 @@ import Link from 'next/link'
  *  "/foo/bar"       for pages/foo/bar.js
  *  "/foo/[...bar]"  for pages/foo/[...bar].js
  */
-const publicPages = ['/', '/sign-in/[[...index]]', '/sign-up/[[...index]]']
+const publicPages = ["/", "/sign-in/[[...index]]", "/sign-up/[[...index]]"];
 
 const MyApp = ({ Component, pageProps }) => {
-  const router = useRouter()
+  const router = useRouter();
 
   /**
    * If the current route is listed as public, render it directly.
@@ -44,10 +44,10 @@ const MyApp = ({ Component, pageProps }) => {
             <SignedOut>
               <main>
                 <p>
-                  Please{' '}
+                  Please{" "}
                   <Link href="/sign-in">
                     <a>sign in</a>
-                  </Link>{' '}
+                  </Link>{" "}
                   to access this page.
                 </p>
               </main>
@@ -56,7 +56,7 @@ const MyApp = ({ Component, pageProps }) => {
         )}
       </Layout>
     </ClerkProvider>
-  )
-}
+  );
+};
 
-export default MyApp
+export default MyApp;
